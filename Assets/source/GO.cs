5cc1c1b729d70c079ba40eaa4b138dc510a9b9d0
takara <takara@corich.co.jp>
2013-08-04 07:04:20 +0900

    視点を上に変更
    不要なコード整理

--- Assets/source/GO.cs
+++ Assets/source/GO.cs
@@ -2,27 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class GO : MonoBehaviour {
+/**
+* ???????
+*/
 	public GameObject boardPrefab;
-	private GameObject board;
-
-	// Use this for initialization
+/**
+* ???
+*/
 	public void Start() {
-		board = (GameObject)Instantiate(boardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+		//GameObject board = (GameObject)
+		Instantiate(boardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
 	}
-	
-/*	private void print_board() {
-		rev.printByAscii();
-		string s;
-		s = "";
-		for(int i2=0; i2<8; i2++){
-			for(int j2=0; j2<8; j2++) {
-				if( true == rev.canPlacePiece(i2, j2) ) {
-					s += (char)('a'+i2);
-					s += (char)('1'+j2);
-					s += " ";
-				}
-			}
-		}
-		((GUIText)GameObject.Find("GUIMessage").GetComponent("GUIText")).text = s;
-	}*/
 }
--- Assets/source/camera.cs
+++ Assets/source/camera.cs
@@ -3,27 +3,51 @@ using System.Collections;
 
 public class camera : MonoBehaviour {
 	void Awake () {
-	  Camera cam = gameObject.GetComponent<Camera>();
-	  float baseAspect = 720f/1280f;
-	  float nowAspect = (float)Screen.height/(float)Screen.width;
-	  float changeAspect;
-	  
-	  if(baseAspect>nowAspect){   
-	   changeAspect = nowAspect/baseAspect;
-	   cam.rect=new Rect((1-changeAspect)*0.5f,0,changeAspect,1);
-	  }else{
-	   changeAspect = baseAspect/nowAspect;
-	   cam.rect=new Rect(0,(1-changeAspect)*0.5f,1,changeAspect);
-	  }
-	  Destroy(this);
+//		_sample2();
 	}
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start () {}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () {}
+/**
+* ????
+*
+* ??????????????
+*/
+	private void _sample1() {
+		Camera cam = gameObject.GetComponent<Camera>();
+		float baseAspect = 720f/1280f;
+		float nowAspect = (float)Screen.height/(float)Screen.width;
+		float changeAspect;
+  
+		if(baseAspect>nowAspect){   
+	 		changeAspect = nowAspect/baseAspect;
+	 		cam.rect=new Rect((1-changeAspect)*0.5f,0,changeAspect,1);
+		} else {
+	 		changeAspect = baseAspect/nowAspect;
+	 		cam.rect=new Rect(0,(1-changeAspect)*0.5f,1,changeAspect);
+		}
+		Destroy(this);
+	}
+/**
+* ????
+*/
+	private void _sample2() {
+		Camera cam = gameObject.GetComponent<Camera>();
+		float baseAspect = 1280f/720f;
+		float nowAspect = (float)Screen.height/(float)Screen.width;
+		float changeAspect;
+Debug.Log("cam:"+cam.rect);
+		changeAspect = nowAspect/baseAspect;  
+ 		cam.rect=new Rect((1-changeAspect)*0.5f,0,changeAspect,1);
+/*		if(baseAspect>nowAspect){   
+	 		changeAspect = nowAspect/baseAspect;
+	 		cam.rect=new Rect((1-changeAspect)*0.5f,0,changeAspect,1);
+		} else {
+	 		changeAspect = baseAspect/nowAspect;
+	 		cam.rect=new Rect(0,(1-changeAspect)*0.5f,1,changeAspect);
+		}*/
+		Destroy(this);
 	}
 }
